cacb8876c508b026497df2b11d95563c95be59f9
Carles <c.morincome.sen@gmail.com>
2023-02-23 15:48:30 +0100

    fix bug tournament

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,5 +1,6 @@
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonButton } from '@ionic/react';
 import React, { useState } from 'react';
+import { shuffle } from '../hooks/utils';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -12,6 +13,7 @@ const Tab1: React.FC = () => {
     startTime?: number;
     endTime?: number;
     score?: string;
+    round: string
   }
 
   enum Result {
@@ -52,30 +54,92 @@ const Tab1: React.FC = () => {
     'Sporting CP',
   ];
 
+  
+
   const createMatch = () => {
-    let team1Index, team2Index;
-    do {
-      team1Index = Math.floor(Math.random() * teams.length);
-      team2Index = Math.floor(Math.random() * teams.length);
-    } while (team1Index === team2Index);
-  
-    const team1 = teams[team1Index];
-    const team2 = teams[team2Index];
-  
-    if (disqualifiedTeams.includes(team1) || disqualifiedTeams.includes(team2)) {
-      console.log("Une ou les deux équipes ont été disqualifiées. Pas de nouveau match.");
+    const teamsLeft = teams.filter((team) => !disqualifiedTeams.includes(team));
+    if (teamsLeft.length < 2) {
+      console.log("Il n'y a pas assez d'équipes pour créer un nouveau match.");
       return;
     }
   
-    const newMatch: Match = {
-      id: Date.now(),
-      team1,
-      team2,
-      startTime: Date.now(),
-      score: '',
-    };
-    setMatches([...matches, newMatch]);
+    const shuffledTeams = shuffle(teamsLeft);
+    const newMatches: Match[] = [];
+  
+    if (matches.length === 0) {
+      for (let i = 0; i < shuffledTeams.length; i += 2) {
+        const team1 = shuffledTeams[i];
+        const team2 = shuffledTeams[i + 1];
+        const newMatch: Match = {
+          id: Date.now() + i,
+          team1,
+          team2,
+          startTime: Date.now(),
+          score: '',
+          round: "Huitiéme de final",
+        };
+        newMatches.push(newMatch);
+      }
+    } else if (matches.length === 8) {
+      const winners = matches.filter(match => match.result === Result.Win).map(match => match.result === Result.Win ? match.team1 : match.team2 );
+      
+      if (winners.length === 1) {
+        alert(`Le vainqueur est ${winners[0]}`);
+        return;
+      }
+  
+      for (let i = 0; i < winners.length; i += 2) {
+        const team1 = winners[i];
+        const team2 = winners[i + 1];
+        const newMatch: Match = {
+          id: Date.now() + i,
+          team1,
+          team2,
+          startTime: Date.now(),
+          score: '',
+          round: "Quarts de finale",
+        };
+        newMatches.push(newMatch);
+      }
+    } else if (matches.length === 4) {
+      const winners = matches.filter(match => match.result === Result.Win).map(match => match.result === Result.Win ? match.team1 : match.team2);
+      
+      if (winners.length === 1) {
+        alert(`Le vainqueur est ${winners[0]}`);
+        return;
+      }
+  
+      for (let i = 0; i < winners.length; i += 2) {
+        const team1 = winners[i];
+        const team2 = winners[i + 1];
+        const newMatch: Match = {
+          id: Date.now() + i,
+          team1,
+          team2,
+          startTime: Date.now(),
+          score: '',
+          round: "Demi-final"
+        };
+        newMatches.push(newMatch);
+      }
+    } else if (matches.length === 2) {
+      // Create Final match
+      const team1 = matches[0].result === Result.Win ? matches[0].team1 : matches[0].team2;
+      const team2 = matches[1].result === Result.Win ? matches[1].team1 : matches[1].team2;
+      const newMatch: Match = {
+        id: Date.now(),
+        team1,
+        team2,
+        startTime: Date.now(),
+        score: '',
+        round: "Finale"
+      };
+      newMatches.push(newMatch);
+    }
+  
+    setMatches([...matches, ...newMatches]);
   };
+
   
 
   const placeBet = (matchId: number, team: string, result: Result) => {
@@ -138,11 +202,6 @@ const Tab1: React.FC = () => {
       const updatedMatches = [...prevMatches];
       updatedMatches[matchIndex] = updatedMatch;
     
-      const remainingTeams = new Set(updatedMatches.filter((match) => !match.endTime && !match.result).map((match) => match.team1));
-      if (remainingTeams.size === 1) {
-        const winner = remainingTeams.values().next().value;
-        alert(`Le vainqueur est ${winner}`);
-      }
     
       const disqualifiedTeams = new Set(updatedMatches.filter((match) => match.result === Result.Lose).map((match) => match.team1));
       if (disqualifiedTeams.size > 0) {
@@ -162,72 +221,67 @@ const Tab1: React.FC = () => {
   
 
 
-  
-  
-  
-  
-  
-
-  
-
-
-
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-        <ExploreContainer name="Tab 1 page" />
-        <IonList>
-  {matches.map(match => (
-   <IonItem key={match.id}>
-   <IonLabel>
-     <h2>
-       {match.team1} vs. {match.team2}
-     </h2>
-     <p>{match.score ? match.score : "Match à venir"}</p>
-   </IonLabel>
-   {match.endTime ? (
-     <IonButton disabled>Match terminé</IonButton>
-   ) : (
-     <IonButton onClick={() => endMatch(match.id)}>Terminer le match</IonButton>
-   )}
-   <IonButton onClick={() => placeBet(match.id, match.team1, Result.Win)}>Parier sur {match.team1}</IonButton>
-   <IonButton onClick={() => placeBet(match.id, match.team2, Result.Win)}>Parier sur {match.team2}</IonButton>
-   <IonButton onClick={() => placeBet(match.id, match.team1,Result.Draw)}>Parier sur match nul</IonButton>
+  <IonHeader>
+    <IonToolbar>
+      <IonTitle>Tab 1</IonTitle>
+    </IonToolbar>
+  </IonHeader>
+  <IonContent>
+    <ExploreContainer name="Tab 1 page" />
+    <IonList>
+      {matches
+        .filter((match) => match.round === "Huitiéme de final" || match.round === "Quarts de finale" || match.round === "Demi-final" || match.round === "Finale") // Filtrer les matchs pour les quarts de finale, les demi-finales et la finale.
+        .map(match => (
+          <IonItem key={match.id}>
+            <IonLabel>
+              <h2>
+                {match.team1} vs. {match.team2}
+              </h2>
+              <p>{match.score ? match.score : "Match à venir"}</p>
+            </IonLabel>
+            {match.endTime ? (
+              <IonButton disabled>Match terminé</IonButton>
+            ) : (
+              <IonButton onClick={() => endMatch(match.id)}>Terminer le match</IonButton>
+            )}
+            <IonButton onClick={() => placeBet(match.id, match.team1, Result.Win)}>Parier sur {match.team1}</IonButton>
+            <IonButton onClick={() => placeBet(match.id, match.team2, Result.Win)}>Parier sur {match.team2}</IonButton>
+            <IonButton onClick={() => placeBet(match.id, match.team1,Result.Draw)}>Parier sur match nul</IonButton>
+          </IonItem>
+        ))}
+    </IonList>
+    <IonList>
+      <IonItem>
+        <IonLabel>
+          <h2>Create a new match</h2>
+        </IonLabel>
+        <IonButton onClick={() => createMatch()}>Create</IonButton>
+      </IonItem>
+    </IonList>
+    <IonList>
+      <IonItem>
+        <IonLabel>
+          <h2>Your bets</h2>
+        </IonLabel>
+      </IonItem>
+      {bets.map(bet => (
+        <IonItem key={bet.id}>
+          <IonLabel>
+            <p>
+              You bet {bet.result} on {bet.team} in the match {bet.matchId}
+            </p>
+          </IonLabel>
+        </IonItem>
+      ))}
+    </IonList>
+  </IonContent>
+</IonPage>
 
- </IonItem>
-  ))}
-</IonList>
-        <IonList>
-          <IonItem>
-            <IonLabel>
-              <h2>Create a new match</h2>
-            </IonLabel>
-            <IonButton onClick={() => createMatch()}>Create</IonButton>
-          </IonItem>
-        </IonList>
-        <IonList>
-          <IonItem>
-            <IonLabel>
-              <h2>Your bets</h2>
-            </IonLabel>
-          </IonItem>
-          {bets.map(bet => (
-            <IonItem key={bet.id}>
-              <IonLabel>
-                <p>
-                  You bet {bet.result} on {bet.team} in the match {bet.matchId}
-                </p>
-              </IonLabel>
-            </IonItem>
-          ))}
-        </IonList>
-      </IonContent>
-    </IonPage>
+
+
+
   );
 };
 export default Tab1;
